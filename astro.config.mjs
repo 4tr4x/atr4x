6704a98298fa9ae316bf97b9319b0214a3f14511
4tr4x <4tr4x888@gmail.com>
2025-10-06 18:12:07 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
 		resolve: {
 			alias: {
 				// Redirige el componente original a tu versión personalizada
-				'@astrojs/': '/src/components/astro',
+				'@astrojs/' : '/src/components/astro',
 			},
 		},
 	},
